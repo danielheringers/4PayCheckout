556f10347490613dc660237432181ad664106b1a
danielheringer <danielheringers@gmail.com>
2024-12-06 13:35:10 -0300

    Disable TypeScript explicit any rule in multiple components

--- components/invoices.tsx
+++ components/invoices.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect } from "react";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
